019619065a53b3d2ee68b598466daa76e46f67c8
santiponssa <tatoponssa19@gmail.com>
2023-05-30 13:26:08 -0300

    jdlqsa

--- ClsRepuestos.cs
+++ ClsRepuestos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,29 +11,37 @@ namespace pryPonssaRepuestos._2
 {
     public class ClsRepuestos
     {
-         //Propiedades
+         //Propiedades del repuesto
          public string Código { get; set; }
          public string Nombre { get; set; }
          public string Marca { get; set; }
-         public string Precio { get; set; }
+         public float Precio { get; set; }
          public string Origen { get; set; }
-        
-         //Métodos
-         public void GuardarRepuesto()
-         {
-            StreamWriter sw = new StreamWriter("REPUESTOS.txt", false);
-            sw.WriteLine(Código + "," + Nombre + "," + Marca + "," + Precio + "," + Origen);
+
+        //Métodos
+        public void GuardarRepuesto()
+        {
+            StreamWriter sw = new StreamWriter("REPUESTOS.txt", true);
+            sw.WriteLine(Código + "," + Nombre + "," + Marca + "," + 
+                         Precio.ToString() + "," + Origen);
             sw.Close();
             sw.Dispose();
-         }
-        public void VerificarCódigo()
+        }
+        public void VerificarCódigoRepetido()
         {
+            string Linea;
             StreamReader sr = new StreamReader("REPUESTOS.txt");
-            sr.ReadToEnd();
-            if (Código == sr.ReadLine())
+            while (sr.EndOfStream == false)
             {
-
+                Linea = sr.ReadLine();
+                if (Código == Linea.Split(',')[0])
+                {
+                    MessageBox.Show("Código de repuesto repetido");
+                    break;
+                }
             }
+            sr.Close();
+            sr.Dispose();
         }
     }
 }
